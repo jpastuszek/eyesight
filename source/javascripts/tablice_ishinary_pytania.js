9ae2301ee36580672b4d5c3a188cc16a34fcb960
Jakub Pastuszek <jpastuszek@gmail.com>
2012-05-21 15:22:57 +0200

    text fixes; removed console logging

--- source/javascripts/tablice_ishinary_pytania.js
+++ source/javascripts/tablice_ishinary_pytania.js
@@ -1,21 +1,21 @@
-var answares = [
+﻿var answares = [
   { 
-    'Liczbe 12': {
-      'description': 'Wszyscy ludzie powinnin widzieć liczbe 12, nawet ci którzy mają problemy z prawidłowym widzeniem kolorów.',
+    'Liczbę 12': {
+      'description': 'Wszyscy ludzie powinni widzieć liczbę 12, nawet ci którzy mają problemy z prawidłowym widzeniem kolorów.',
       'status': 'success'
     },
     'Nic': {
-      'description': 'Na prawde nie widzisz tej dwunastki?! :)',
+      'description': 'Na prawdę nie widzisz tej dwunastki?! :)',
       'status': 'info'
     }
   },
   {
-    'Liczbe 8': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 8.',
+    'Liczbę 8': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 8.',
       'status': 'success'
     },
-    'Liczbe 3': {
-      'description': 'Ci którzy mają ślepotę na kolory czerwony i zielony widzą liczbe 3.',
+    'Liczbę 3': {
+      'description': 'Ci którzy mają ślepotę na kolory czerwony i zielony widzą liczbę 3.',
       'status': 'info'
     },
     'Nic': {
@@ -24,12 +24,12 @@ var answares = [
     }
   },
   { 
-    'Liczbe 29': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 19.',
+    'Liczbę 29': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 29.',
       'status': 'success'
     },
-    'Liczbe 70': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbe 70',
+    'Liczbę 70': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbę 70',
       'status': 'info'
     },
     'Nic': {
@@ -38,12 +38,12 @@ var answares = [
     }
   },
   { 
-    'Liczbe 5': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 5.',
+    'Liczbę 5': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 5.',
       'status': 'success'
     },
-    'Liczbe 2': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbe 2',
+    'Liczbę 2': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbę 2',
       'status': 'info'
     },
     'Nic': {
@@ -52,12 +52,12 @@ var answares = [
     }
   },
   { 
-    'Liczbe 3': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 3.',
+    'Liczbę 3': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 3.',
       'status': 'success'
     },
-    'Liczbe 5': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbe 5',
+    'Liczbę 5': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbę 5',
       'status': 'info'
     },
     'Nic': {
@@ -66,12 +66,12 @@ var answares = [
     }
   },
   { 
-    'Liczbe 15': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 15.',
+    'Liczbę 15': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 15.',
       'status': 'success'
     },
-    'Liczbe 17': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbe 17',
+    'Liczbę 17': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbę 17',
       'status': 'info'
     },
     'Nic': {
@@ -80,12 +80,12 @@ var answares = [
     }
   },
   { 
-    'Liczbe 74': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 74.',
+    'Liczbę 74': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 74.',
       'status': 'success'
     },
-    'Liczbe 21': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbe 21',
+    'Liczbę 21': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbę 21',
       'status': 'info'
     },
     'Nic': {
@@ -94,110 +94,110 @@ var answares = [
     }
   },
   { 
-    'Liczbe 6': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 6.',
+    'Liczbę 6': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 6.',
       'status': 'success'
     },
     'Nic': {
-      'description': 'Większośc ludzi którzy mają ślepotę na kolory nie widzą tegu numeru wyraźnie.',
+      'description': 'Większość ludzi którzy mają ślepotę na kolory nie widzi numeru wyraźnie.',
       'status': 'info'
     }
   },
   {  // 9
-    'Liczbe 45': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 45.',
+    'Liczbę 45': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 45.',
       'status': 'success'
     },
     'Nic': {
-      'description': 'Większośc ludzi którzy mają ślepotę na kolory nie widzą tegu numeru wyraźnie.',
+      'description': 'Większość ludzi którzy mają ślepotę na kolory nie widzi numeru wyraźnie.',
       'status': 'info'
     }
   },
   {  // 10
-    'Liczbe 5': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 5.',
+    'Liczbę 5': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 5.',
       'status': 'success'
     },
     'Nic': {
-      'description': 'Większośc ludzi którzy mają ślepotę na kolory nie widzą tegu numeru wyraźnie.',
+      'description': 'Większość ludzi którzy mają ślepotę na kolory nie widzi numeru wyraźnie.',
       'status': 'info'
     }
   },
   {  // 11
-    'Liczbe 7': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 7.',
+    'Liczbę 7': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 7.',
       'status': 'success'
     },
     'Nic': {
-      'description': 'Większośc ludzi którzy mają ślepotę na kolory nie widzą tegu numeru wyraźnie.',
+      'description': 'Większość ludzi którzy mają ślepotę na kolory nie widzi numeru wyraźnie.',
       'status': 'info'
     }
   },
   {  // 12
-    'Liczbe 16': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 16.',
+    'Liczbę 16': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 16.',
       'status': 'success'
     },
     'Nic': {
-      'description': 'Większośc ludzi którzy mają ślepotę na kolory nie widzą tegu numeru wyraźnie.',
+      'description': 'Większość ludzi którzy mają ślepotę na kolory nie widzi numeru wyraźnie.',
       'status': 'info'
     }
   },
   {  // 13
-    'Liczbe 73': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 73.',
+    'Liczbę 73': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 73.',
       'status': 'success'
     },
     'Nic': {
-      'description': 'Większośc ludzi którzy mają ślepotę na kolory nie widzą tegu numeru wyraźnie.',
+      'description': 'Większość ludzi którzy mają ślepotę na kolory nie widzi numeru wyraźnie.',
       'status': 'info'
     }
   },
   {  // 14
     'Nic': {
-      'description': 'Ci którzy nie mają całkowitą ślepotę na kolory bądź widzą normalnie nie widzą żadnej liczby.',
+      'description': 'Ci którzy mają całkowitą ślepotę na kolory bądź widzą normalnie nie widzą żadnej liczby.',
       'status': 'success'
     },
-    'Liczbe 5': {
-      'description': 'Ci którzy mają ślepotę na kolory czerwony i zielony widzą liczbę 5.',
+    'Liczbę 5': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbę 5.',
       'status': 'info'
     }
   },
   {  // 15
     'Nic': {
-      'description': 'Ci którzy nie mają całkowitą ślepotę na kolory bądź widzą normalnie nie widzą żadnej liczby.',
+      'description': 'Ci którzy mają całkowitą ślepotę na kolory bądź widzą normalnie nie widzą żadnej liczby.',
       'status': 'success'
     },
-    'Liczbe 45': {
-      'description': 'Ci którzy mają ślepotę na kolory czerwony i zielony widzą liczbę 45.',
+    'Liczbę 45': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony i zielony widzą liczbę 45.',
       'status': 'info'
     }
   },
   {  // 16
-    'Liczbe 26': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 26.',
+    'Liczbę 26': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 26.',
       'status': 'success'
     },
-    'Liczbe 6 lub słabo liczbe 2': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony (protanopia) widzą liczbę 6, a ci którzy mają slepotę na słaby kolor czerwony (prontanomaly) zobaczą także słabą liczbę 2',
+    'Liczbę 6 lub słabo liczbę 2': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony (protanopia) widzą liczbę 6, a ci którzy mają ślepotę na słaby kolor czerwony (protanomalia) zobaczą także słabą liczbę 2',
       'status': 'info'
     },
-    'Liczbe 2 lub słabo liczbe 6': {
-      'description': 'Ci którzy mają ślepotę na kolor zielony (deuteranopia) widzą liczbę 2, a ci którzy mają slepotę na słaby kolor zielony (deuteranomaly) mogą także zobaczyć liczbę 6',
+    'Liczbę 2 lub słabo liczbę 6': {
+      'description': 'Ci którzy mają ślepotę na kolor zielony (deuteranopia) widzą liczbę 2, a ci którzy mają ślepotę na słaby kolor zielony (deuteranomalia) mogą także zobaczyć liczbę 6',
       'status': 'info'
     }
   },
   {  // 17
-    'Liczbe 42': {
-      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbe 42.',
+    'Liczbę 42': {
+      'description': 'Ci którzy nie mają problemu z widzeniem kolorów widzą liczbę 42.',
       'status': 'success'
     },
-    'Liczbe 2 lub słabo liczbe 4': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony (protanopia) widzą liczbę 2, a ci którzy mają slepotę na słaby kolor czerwony (prontanomaly) zobaczą także słabą liczbę 4',
+    'Liczbę 2 lub słabo liczbę 4': {
+      'description': 'Ci którzy mają ślepotę na kolor czerwony (protanopia) widzą liczbę 2, a ci którzy mają ślepotę na słaby kolor czerwony (protanomalia) zobaczą także słabą liczbę 4',
       'status': 'info'
     },
-    'Liczbe 4 lub słabo liczbe 2': {
-      'description': 'Ci którzy mają ślepotę na kolor zielony (deuteranopia) widzą liczbę 4, a ci którzy mają slepotę na słaby kolor zielony (deuteranomaly) mogą także zobaczyć liczbę 2',
+    'Liczbę 4 lub słabo liczbę 2': {
+      'description': 'Ci którzy mają ślepotę na kolor zielony (deuteranopia) widzą liczbę 4, a ci którzy mają ślepotę na słaby kolor zielony (deuteranomalia) mogą także zobaczyć liczbę 2',
       'status': 'info'
     }
   },
@@ -207,11 +207,11 @@ var answares = [
       'status': 'success'
     },
     'Szlaczek różowy i/lub słabo szlaczek czerwony': {
-      'description': 'Ci którzy mają ślepotę na kolor czerwony (protanopia) powinni być w stanie prześledzić szlaczek różowy, a ci którzy mają slepotę na słaby kolor czerwony (prontanomaly) mogą z trudnościami być w stanie prześledzić szlaczek czerwony',
+      'description': 'Ci którzy mają ślepotę na kolor czerwony (protanopia) powinni być w stanie prześledzić szlaczek różowy, a ci którzy mają ślepotę na słaby kolor czerwony (protanomalia) mogą z trudnościami być w stanie prześledzić szlaczek czerwony',
       'status': 'info'
     },
     'Szlaczek czerwony i/lub słabo szlaczek różowy': {
-      'description': 'Ci którzy mają ślepotę na kolor zielony (deuteranopia) powinni być w stanie prześledzić szlaczek czerwony, a ci którzy mają slepotę na słaby kolor zielony (deuteranomaly) mogą z trudnościami być w stanie prześledzić szlaczek różowy',
+      'description': 'Ci którzy mają ślepotę na kolor zielony (deuteranopia) powinni być w stanie prześledzić szlaczek czerwony, a ci którzy mają ślepotę na słaby kolor zielony (deuteranomalia) mogą z trudnościami być w stanie prześledzić szlaczek różowy',
       'status': 'info'
     }
   },
@@ -221,7 +221,7 @@ var answares = [
       'status': 'success'
     },
     'Wijący się szlaczek': {
-      'description': 'Większość ludzi z ślepotą na kolory czerwony i zielony powinni być w stanie prześledzić wijący szlaczek, w zależności od stopnia zaawansowania schorzenia',
+      'description': 'Większość ludzi z ślepotą na kolory czerwony i zielony powinno być w stanie prześledzić wijący się szlaczek, w zależności od stopnia zaawansowania schorzenia',
       'status': 'info'
     },
   },
@@ -231,7 +231,7 @@ var answares = [
       'status': 'success'
     },
     'Brak wyraźnego szlaczka': {
-      'description': 'Większość ludzi z jakąklowiek ślepotą na kolory nie powinni być w stanie prześledzić szlacza',
+      'description': 'Większość ludzi z jakąkolwiek ślepotą na kolory nie powinno być w stanie prześledzić szlaczka',
       'status': 'info'
     },
   },
@@ -241,7 +241,7 @@ var answares = [
       'status': 'success'
     },
     'Brak wyraźnego szlaczka': {
-      'description': 'Większość ludzi z jakąklowiek ślepotą na kolory nie powinni być w stanie prześledzić szlacza',
+      'description': 'Większość ludzi z jakąkolwiek ślepotą na kolory nie powinno być w stanie prześledzić szlaczka',
       'status': 'info'
     },
   },
@@ -265,7 +265,7 @@ var answares = [
       'status': 'success'
     },
     'Niebiesko-zielony i czerwony szlaczek': {
-      'description': 'Ci którzy mają problem z widzeniem kolorów czerwonego i zielonego będą w stanie prześledzić czerwony i biebiesko-zielony szlaczek',
+      'description': 'Ci którzy mają problem z widzeniem kolorów czerwonego i zielonego będą w stanie prześledzić czerwony i niebiesko-zielony szlaczek',
       'status': 'info'
     },
     'Brak wyraźnego szlaczka': {
@@ -279,7 +279,7 @@ var answares = [
       'status': 'success'
     },
     'Nic': {
-      'description': 'Na prawde nie widzisz tego szlaczka?! :)',
+      'description': 'Na prawdę nie widzisz tego szlaczka?! :)',
       'status': 'info'
     },
   }
